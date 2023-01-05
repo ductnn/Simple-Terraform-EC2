eb52e397fa01321a784f93ac093696ca2315941f
ductnn <trannhuduc531998@gmail.com>
2023-01-05 11:30:39 +0700

    update

--- dev/provider.tf
+++ dev/provider.tf
@@ -1,5 +1,5 @@
-# provider "aws" {
-#   access_key = var.common_aws_access_access_key
-#   secret_key = var.common_aws_access_secret_key
-#   region     = var.common_aws_region
-# }
+provider "aws" {
+  # access_key = var.common_aws_access_access_key
+  # secret_key = var.common_aws_access_secret_key
+  region     = "us-east-1"
+}
