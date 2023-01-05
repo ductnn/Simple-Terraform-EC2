246f78f6bae97e9fcca2083f9ad1c983d498ce9e
ductnn <trannhuduc531998@gmail.com>
2023-01-05 15:21:31 +0700

    test

---
+++ cicd/Jenkinsfile.groovy
@@ -0,0 +1,44 @@
+node {
+    withAWS(credentials: 'aws-demo-credential', region: 'us-east-1') {
+        ansiColor('xterm') {
+            checkout()
+            init()
+            lint()
+            plan()
+        }
+    }
+}
+
+def checkout() {
+    stage('Clone') {
+        // git branch: 'master', url: 'https://github.com/ductnn/Simple-Terraform-EC2.git'
+        checkout scm
+    }
+}
+
+def init() {
+    stage('Init') {
+        dir ("dev") {
+            sh 'ls && whoami'
+            sh 'terraform init'
+        }
+    }
+}
+
+def lint() {
+    stage('Lint') {
+        dir ("dev") {
+            sh 'terraform fmt'
+            sh 'terraform validate'
+        }
+    }
+}
+
+def plan() {
+    stage('Plan') {
+        dir ("dev") {
+            sh 'terraform plan'
+            sh 'printenv'
+        }
+    }
+}
